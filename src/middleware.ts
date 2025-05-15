38fbc8283b9216b8fa26aaaa72fbaf5dd137e582
sopnilali <sopnilali24@gmail.com>
2025-05-15 21:58:28 +0600

    aded

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,7 +28,7 @@ export const middleware = async (request: NextRequest) => {
     } else {
       return NextResponse.redirect(
         new URL(
-          `http://localhost:3000/login?redirectPath=${pathname}`,
+          `https://hello-shop-eight.vercel.app/login?redirectPath=${pathname}`,
           request.url
         )
       );
